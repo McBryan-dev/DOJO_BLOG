dabe3362f218a26335e0ffd8122a9aeb2ff39ff6
McBryan-dev <bryanokononfua@gmail.com>
2024-02-04 13:02:09 +0000

    Next Commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,72 @@
 <template>
   <div class="home">
-    Home
+    <h2>My Name is {{name}} and i am {{age}} years old</h2>
+    <br />
+    <strong>{{full}}</strong>
+    <br />
+    <button @click="handleClick">darega watashi wa</button>
+    <button @click="age++">Plus One</button>
+    <input type="text" v-model="name" />
+    <br />
+    <button @click="fetchApi" >Save</button>
   </div>
 </template>
+
 <script>
+
+  import {ref} from 'vue';
+
   export default {
-    name: 'home'
+
+    name: 'home',
+
+    setup() {
+      const  name= ref("mario")
+      const age = ref(30)
+
+      const full = ref("Not Saved")
+
+      const handleClick = () => {
+        name.value= 'luigi'
+        age.value = 35
+      }
+
+      const fetchApi = async () => {
+        try {
+
+          const newData = {
+            name: name.value,
+            age: age.value
+          }
+
+          const res = await fetch (' http://localhost:3000/posts', {
+            method: 'POST',
+            headers: {
+              'Content-type': 'application/json'
+            },
+            body: JSON.stringify(newData)
+          })
+
+          const data = res.json();
+
+          if(data) {
+            
+          }
+        
+        } catch(error) {
+          console.log(error)
+        }
+      }
+
+      return {
+        name,
+        age,
+        full,
+        handleClick,
+        fetchApi
+      }
+
+    }
+
   }
 </script>
