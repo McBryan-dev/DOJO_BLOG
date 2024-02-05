f5625c9f104e9b8765d258bb8e1dfe3abcf68713
McBryan-dev <bryanokononfua@gmail.com>
2024-02-05 22:45:16 +0000

    Next Commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,23 @@
 <template>
-  <div class="home">
-    <h2>My Name is {{name}} and i am {{age}} years old</h2>
+
+  <div class="home p-4 m-2 justify-center align-center text-center gap-2 flex flex-col bg-slate-300 rounded">
+    <h2 class="text-green-500 text-3xl" >My Name Is {{name}} And I Am {{age}} Years Of Age</h2>
     <br />
-    <strong>{{full}}</strong>
+    <div class="bg-blue-300 py-1 w-32 mx-auto rounded justify-center text-center align-center" >
+      <strong>{{full}}</strong>
+    </div>
     <br />
-    <button @click="handleClick">darega watashi wa</button>
-    <button @click="age++">Plus One</button>
-    <input type="text" v-model="name" />
+    <div class="flex justify-center text-center align-center">
+      <button @click="handleClick" class="bg-neutral-200 w-36 rounded text-black-200 mx-auto p-1 border-2 border-neutral-300" >Set name</button>
+      <button @click="age++, changeVal" class="mx-auto bg-neutral-400 rounded text-back-200 p-1 px-2 border-2 border-neutral-300">Plus One Year</button>
+    </div>
     <br />
-    <button @click="fetchApi" >Save</button>
+    <input type="text" class="w-1/2 mx-auto p-1 text-lg rounded" v-model="name" id="exampleInput" @input="setFull" />
+    <br />
+    <button @click="postApi" class="bg-red-400 w-1/4 mx-auto p-2 rounded">Save</button>
+    <button @click="fetchApi" class="bg-red-400 w-1/4 mx-auto p-2 rounded" >View Saved</button>
   </div>
+
 </template>
 
 <script>
@@ -31,7 +39,17 @@
         age.value = 35
       }
 
-      const fetchApi = async () => {
+      const setFull = (e) => {
+        e.target.id = e.target.value;
+        changeVal();
+      }
+
+      const changeVal = () => {
+        full.value = 'Not Saved'
+        console.log('val')
+      }
+
+      const postApi = async () => {
         try {
 
           const newData = {
@@ -49,8 +67,12 @@
 
           const data = res.json();
 
-          if(data) {
-            
+          if(!data) {
+            console.log(error)
+            full.value = 'Not Saved';
+          } else {
+            console.log(data);
+            full.value = 'Saved';
           }
         
         } catch(error) {
@@ -63,7 +85,9 @@
         age,
         full,
         handleClick,
-        fetchApi
+        changeVal,
+        setFull,
+        postApi
       }
 
     }
