210714a33fdbc8d2558ebf3a2f1c7bffc8e1764f
McBryan-dev <bryanokononfua@gmail.com>
2024-02-09 22:26:13 +0000

    Last Comitm

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,6 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    host: '0.0.0.0',
-    allowedHosts: 'all',
-    disableHostCheck: true
+    allowedHosts: 'all'
   }
 })
